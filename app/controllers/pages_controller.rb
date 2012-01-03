46b0e8f0cc0bbfecbe8f60cd6d7f3c300c440dee
swathi <swathi@weboniselab.com>
2012-01-03 23:19:33 +0530

    created sessions,callback

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+    @title = "Home"
+  end
+
+  def contact
+    @title = "Contact"
+  end
+    def help
+    @title = "Help"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3d039b3a148fffbf4d5d2844d5a2e73bf665f8b25a2fbcb83b53fbe58bc094fe1fa21ca87640c0d90fc5f5104b9ee683efae0a036fbbbbce70b268df79b9dd27'
